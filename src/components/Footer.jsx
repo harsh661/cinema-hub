64c09efe4bd837179bc6e443188bac9ffc8e6dc2
harsh661 <plasmaharsh@gmail.com>
2023-07-18 11:01:15 +0530

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='p-10 mt-10 bg-dark-gray'>
-
+    <div className='p-10 mt-10 bg-dark-gray text-dark-text text-center text-sm'>
+      <span>© 2023 Harsh Raj</span>
     </div>
   )
 }
