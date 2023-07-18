87dfde28fc96e996308d30e8eea0d8c54e3c678d
harsh661 <plasmaharsh@gmail.com>
2023-07-18 10:35:50 +0530

    ui enhancement

--- src/components/CompanyCard.jsx
+++ src/components/CompanyCard.jsx
@@ -3,13 +3,13 @@ import React from "react"
 const CompanyCard = ({ name, logo, country }) => {
   if (!logo) return
   return (
-    <div className="flex flex-col items-center gap-2">
+    <div className="flex flex-col gap-2 w-14 md:w-20">
       <img
         src={`https://image.tmdb.org/t/p/original${logo}`}
         alt={name}
         className="w-14 h-14 md:w-20 md:h-20 rounded-full object-contain bg-white"
       />
-      <span className="text-xs sm:text-sm text-white whitespace-nowrap">{name}</span>
+      <h2 className="truncate overflow-hidden overflow-ellipsis text-xs sm:text-sm text-white">{name}</h2>
     </div>
   )
 }
