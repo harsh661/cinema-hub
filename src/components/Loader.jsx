6f067e552ca4ea294f50ef731316a534057491d6
harsh661 <plasmaharsh@gmail.com>
2023-07-18 10:56:33 +0530

    add bg color to loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import {RiLoader4Line} from 'react-icons/ri'
 
 const Loader = () => {
   return (
-    <div className='absolute inset-0 flex items-center justify-center'>
+    <div className='absolute inset-0 flex items-center justify-center bg-main-bg'>
         <span className='animate-spin'>
           <RiLoader4Line fill='#64748B' size={40}/>
         </span>
