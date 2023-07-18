b4fa2ef7227e8043f302e396d20f156685f3b503
harsh661 <plasmaharsh@gmail.com>
2023-07-18 14:30:42 +0530

    add trending people on home page

--- contexts/moviesContext.jsx
+++ contexts/moviesContext.jsx
@@ -5,9 +5,10 @@ export const MoviesContext = createContext([])
 const MoviesContextProvider = ({children}) => {
     const [movies, setMovies] = useState([])
     const [tv, setTv] = useState([])
+    const [people, setPeople] = useState([])
     const [searchQuery, setSearchQuery] = useState('')
     return(
-        <MoviesContext.Provider value={{movies, setMovies, tv, setTv, searchQuery, setSearchQuery}}>
+        <MoviesContext.Provider value={{movies, setMovies, tv, setTv, searchQuery, setSearchQuery, people, setPeople}}>
             {children}
         </MoviesContext.Provider>
     )
--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import { useContext, useEffect, useState } from "react"
 import { MoviesContext } from "../contexts/moviesContext.jsx"
 import SearchPage from "./pages/SearchPage.jsx"
 import Loader from "./components/Loader.jsx"
+import ArtistCard from "./components/ArtistCard.jsx"
 
 function App() {
-  const { movies, setMovies, tv, setTv, searchQuery } =
+  const { movies, setMovies, tv, setTv, searchQuery, people, setPeople } =
     useContext(MoviesContext)
   const [random, setRandom] = useState(0)
+
   useEffect(() => {
     if (movies.length === 0) {
       const url = `https://api.themoviedb.org/3/trending/movie/day`
@@ -34,6 +36,13 @@ function App() {
         .then((res) => res.json())
         .then((json) => setTv(json.results))
         .catch((err) => console.error("error:" + err))
+
+      const urlforPeople = `https://api.themoviedb.org/3/trending/person/day`
+
+      fetch(urlforPeople, options)
+        .then((res) => res.json())
+        .then((json) => setPeople(json.results))
+        .catch((err) => console.error("error:" + err))
     }
 
     const randomNumber = Math.floor(Math.random() * 20)
@@ -52,17 +61,24 @@ function App() {
         link={`/movie/${movies[random]?.id}`}
         title={movies[random]?.title}
       />
-      <Heading title="Trending in Movies" />
+      <Heading title="Trending Movies" />
       <Grid>
         {movies.map((movie) => (
           <MovieCard key={movie.id} {...movie} />
         ))}
       </Grid>
 
-      <Heading title="Trending in TV" />
+      <Heading title="Trending TV shows" />
       <Grid>
         {tv.map((movie) => (
           <MovieCard key={movie.id} {...movie} />
+        ))}
+      </Grid>
+
+      <Heading title="Trending People" />
+      <Grid>
+        {people.map((artist) => (
+          <ArtistCard key={artist.id} {...artist} />
         ))}
       </Grid>
     </Container>
